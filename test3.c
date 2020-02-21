33ba44f84f1d10dd666ee33252fb2a5a0a6e5fab
geeky-wizard <f20170225@pilani.bits-pilani.ac.in>
2020-02-22 00:35:09 +0530

    grammarStructUpdate

--- test3.c
+++ test3.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "lexerDef.h"
+#include <stdbool.h>
 
 #define NTSIZE 	30
 #define TSIZE	20
@@ -9,25 +11,48 @@
 
 char buffer[BUFF_SIZE];
 
-struct rule{
-    token nt;
-    token** heads;
+struct ruleToken{
+    int tag; // Non-terminal(0) or terminal(1)
+    char tnt[30];
+    struct ruleToken* next;
 };
 
-struct firstFollow{
-    token nt;
-    token** firsts;
-    token** follows;
+struct ntRules{
+    char nt[NTSIZE];
+    int numRules;
+    struct ruleToken* heads;
 };
 
+struct ntfirstFollow{
+    char nt[NTSIZE];
 
-struct rule grammar[NON_TERMINALS];
-struct firstFollow FirstFollowSets[NON_TERMINALS];
+    char* firsts[TSIZE]; // firsts consists only of terminals
+    int numFirsts;
 
-// void ComputeFirstAndFollow(grammar G,FirstandFollow F)
-// {
-    
-// }
+    char* follows[TSIZE]; // follows consists only of terminals
+    int numRules;
+};
+
+struct ntRules grammar[NON_TERMINALS];
+int grammarLength = 0;
+
+struct ntfirstFollow FirstFollowSets[NON_TERMINALS];
+
+void ComputeFirstAndFollow()
+{
+    for(int i=0;i<grammarLength;i++){
+
+        struct ntRules currentRules = grammar[i];
+
+        token* currentNt = &currentRules.nt;
+
+        for(int j=0;j<currentRules.numRules;j++){
+            
+            FirstFollowSets[i] = 
+        }
+
+    }
+}
 
 
 int main(){
@@ -35,12 +60,21 @@ int main(){
     FILE* fp = fopen("grammar.txt","r");
 
     if (fp == NULL) {
-        printf("Error Opening Grammer file");
+        printf("Error Opening Grammar file");
         // Program exits if file pointer returns NULL.
         exit(1);
     }else{
+
+        bool alternateLine = 0;
+
         while(fgets(buffer, BUFF_SIZE, fp)) {
+            buffer[strcspn(buffer, "\n")] = 0;
+
+            if(alternateLine)
             printf("%s\n", buffer);
+
+            alternateLine = !alternateLine;
+            // break;
         }
     }
 
