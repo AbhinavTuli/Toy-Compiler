f7a9c2c1f7a2712e7b56a2d2bad0c35ac69989af
amratanshu <anshushrivastava.shrivastava@gmail.com>
2020-02-22 18:19:07 +0530

    CreateTable draft 2

--- parseTableTest.c
+++ parseTableTest.c
@@ -1,22 +1,8 @@
+#include<string.h>
+#include<stdio.h>
 #include "parser.h"
-// Hash Function for strings
-// unsigned long hash(unsigned char *str)
-//     {
-//         unsigned long hash = 5381;
-//         int c;
-//         while (c = *str++)
-//             hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-//             return hash;
-//     }
-int Table[MAX_NON_TERMINALS][TERMINALS]; //stores the rule number
 
 
-
-void ComputeFirstAndFollow(grammar G,FirstandFollow F)
-{
-                            // if first is terminal, return first, else recursively go through non terminals
-
-}
 
 void createParseTable(struct ntfirstFollow firstFollowSets[MAX_NON_TERMINALS], struct ntRules grammar[MAX_TERMINALS]) //, int[][] Table)
 {
@@ -88,13 +74,3 @@ void createParseTable(struct ntfirstFollow firstFollowSets[MAX_NON_TERMINALS], s
         }
     }
  }
-
-parseInputSourceCode(char *testcaseFile, table T)
-{
-
-}
-
-printParseTree(parseTree PT, char *outfile)
-{
-
-}
