9b5460b66c80a8e8d3835725492d94b7c3bdd962
Kushagra Raina <kushagra.raina@gmail.com>
2020-04-10 18:14:11 +0530

    adding table entry structures

--- Symbol Table/constructST.h
+++ Symbol Table/constructST.h
@@ -3,6 +3,61 @@
 #include "table_rec.h"
 #include "table_func.h"
 
+typedef struct variable_Entry
+{
+    char map[20];                                          // key in the hash table will be mapped to the name
+    char dtype[20];                                         // data type of variable 
+    int check;                                              // check if given data type is an array
+    int range[2];
+    int size; 
+    int offset; 
+}var_Entry;
+
+typedef struct function_Entry
+{
+    char map[20];
+    char ret_type[20];
+    
+}func_Entry;
+
+
+
+typedef struct globalTable{
+    TableEntry *head; 
+}GT;
+
+typedef struct table_Entry{
+    char map[20];
+    bool check;                                             // check for variabl/function 
+    entry 
+}tableEntry;
+
+typedef union entry { 
+    variable_Entry var; 
+    function_Entry func;
+}entry; 
+
+
+typedef struct function_table{
+
+
+}function_table;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 typedef struct _globalVar {
 	char* id;
 	char* type;
@@ -10,6 +65,7 @@ typedef struct _globalVar {
 	struct _globalVar *next;
 }globalVar;
 
+// void varLocal(astNode *f);
 void typeExtractionLocalVars(astNode *f);
 /*fills local ID table of passed function
 */
@@ -21,7 +77,7 @@ ERRORS detected
 ignores everything else (some local variable having same name as a global will be caught in typeExtractionID)
 */
 
-void typeExtractionRec(astNode *ast);
+// void typeExtractionRec(astNode *ast);
 /*extracts record info and fills record table
 ERRORS detected
 1) record re-defintion
@@ -36,7 +92,7 @@ ERRORS detected:
 2) input or output parameter has same name as a global variable
 */
 
-void printExtractedRecs();
+// void printExtractedRecs();
 void printGlobalVarList();
 void printFuncs(astNode * root);
 #endif
