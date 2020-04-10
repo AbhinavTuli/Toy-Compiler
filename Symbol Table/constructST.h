d1db780951aee93121ee2388516d170a4003b3b6
Kushagra Raina <kushagra.raina@gmail.com>
2020-04-10 18:23:31 +0530

    added comments

--- Symbol Table/constructST.h
+++ Symbol Table/constructST.h
@@ -1,18 +1,24 @@
-#include "ast.h"
-#include "table_id.h"
-#include "table_rec.h"
+// #include "ast.h"
+// #include "table_id.h"
+// #include "table_rec.h"
 #include "table_func.h"
 
+
+
+
+//for a variable entry in the ST(global or local), 
 typedef struct variable_Entry
 {
-    char map[20];                                          // key in the hash table will be mapped to the name
-    char dtype[20];                                         // data type of variable 
-    int check;                                              // check if given data type is an array
+    char map[20];                           // use hash function to map a variable in a symbol table, mapping will be done with the help of name of variable (and *scope)
+    char dtype[20];                         // data type of variable 
+    int check;                              // check if given data type is an array
     int range[2];
     int size; 
     int offset; 
 }var_Entry;
 
+
+// for a function entry in a ST(global or local), this entry points to the funtion's local symbol table : 
 typedef struct function_Entry
 {
     char map[20];
@@ -21,26 +27,31 @@ typedef struct function_Entry
 }func_Entry;
 
 
+// typedef struct globalTable{
+//     TableEntry *head; 
+// }GT;
 
-typedef struct globalTable{
-    TableEntry *head; 
-}GT;
 
 typedef struct table_Entry{
     char map[20];
     bool check;                                             // check for variabl/function 
-    entry 
+    entry e; 
 }tableEntry;
 
+
+// Entry in table(can be global/local)
 typedef union entry { 
     variable_Entry var; 
     function_Entry func;
 }entry; 
 
 
+// Local Function table : 
 typedef struct function_table{
 
-
+    linkedListFunc *bins;											// Purpose??? linkedlist of functions? or nesting? 
+	int size;          	 											// number of bins - size of all elements?
+	int total;          											// total number of elements
 }function_table;
 
 
